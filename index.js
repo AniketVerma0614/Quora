c0d17981c50e8794a21b4ae8b7df3e932bb0a6d8
AniketVerma0614 <aniketverma0614@gmail.com>
2024-10-04 09:45:07 +0530

    Update  index.js
    Some minor Fixes !!!

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
-//index.js
 const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
 
-// Middleware to parse URL-encoded data
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-// Set the view engine to EJS
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views engine","ejs");
+app.set("views", path.join(__dirname,"views"));
 
-// Serve static files from the 'public' folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname,"public")));
+
 
 let posts = [
-    {
-        username: "apnacollege",
-        content: "I love coding",
-    },
-    {
-        username: "Aniket",
-        content: "It's never too late until you have decided!!!",
-    },
-    {
-        username: "Rahul Kumar",
-        content: "I got selected for my 1st internship!!!",
-    },
-];
+		{
+				username : "apnacollege",
+				content : "I love Coding!"
+		},
+		
+		{
+				username : "SharadhaKhapra",
+				content : "HardWork is Important to achieve success !!!"
+		},
+		
+		{
+				username : "rahulKumar",
+				content : "I got selected for my first Internship !!!"
+		},
+]
 
-// Route to render the 'index' page
-app.get("/posts", (req, res) => {
-    res.render("index", { posts }); // Pass the 'posts' variable to the view
+//Now there is an need to create an 
+//API ==>Lets see how we can do it Anyways !!!
+
+app.get("/posts",(res,req)=>{
+		req.render("index.ejs",{posts});
 });
 
-app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
+
+
+
+app.get("/",(req,res) =>{
+	res.send("server working well !!!");
+});
+
+app.listen(port, ()=>{
+		console.log("lsitening to port : 8080");
 });
