9020b88c2bf29a39615c0b34f8d70c0cdbabcd23
AniketVerma0614 <aniketverma0614@gmail.com>
2024-10-05 10:59:56 +0530

    feat: Add edit functionality for posts with PATCH method
    - Added route to edit posts by ID.
    - Integrated a form on the edit page to update post content.
    - Implemented method-override to handle PATCH requests from forms.
    - Redirects user to the posts page after updating the post content.

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ const { v4: uuidv4 } = require('uuid');
 
 // uuidv4(); 
 // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+//Now will require the package ==> to override the ==> PATCH and request to the ==> GET and POST request !!!
+const methodOverride = require("method-override");
 
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride("_method"));
+app.use(express.json());  // Add this line to parse JSON data
 
 app.set("views engine","ejs");
 app.set("views", path.join(__dirname,"views"));
@@ -74,7 +79,30 @@ app.get("/posts/:id",(req,res)=>{
 });
 
 
+//Now here we will create a patch Request,to Update an specific posts !!!
 
+app.patch("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    console.log(id);
+	let newContent = req.body.content;
+	let post = posts.find((p)=> id ===p.id);
+	post.content = newContent;
+
+	console.log(post);
+	console.log(newContent);
+
+    // res.send("patch request working !!!");
+	res.redirect("/posts");
+});
+
+
+//Creating Route ==> To edit the posts ...
+app.get("/posts/:id/edit",(req,res)=>{
+	let {id} = req.params;
+	let post = posts.find((p)=> id ===p.id);
+
+	res.render("edit.ejs",{post});
+});
 
 app.listen(port, ()=>{
 		console.log("lsitening to port : 8080");
